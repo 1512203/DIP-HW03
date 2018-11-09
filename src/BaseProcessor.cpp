0c3fe436adee7b517eac7c8d443eb8879b8cae8e
1512203 <lawrencealgorithm@gmail.com>
2018-11-09 22:22:07 +0700

    DIP03: Implement bicubic interpolation

--- src/BaseProcessor.cpp
+++ src/BaseProcessor.cpp
@@ -14,16 +14,88 @@ void BaseProcessor::execute(argv_t kwargs) {
 }
 
 
+double BaseProcessor::getF(int x, int y, double f[][4]) {
+    return f[x+1][y+1];
+}
+
+
+void BaseProcessor::calculateFeatureVector(double f[][4], double g[16]) { 
+    int id = 0;
+    for (int i = 0; i < 4; ++i) {
+        g[id++] = this->getF(i/4, i%4, f);
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            g[id++] = this->getF(i+1, j, f)-this->getF(i-1, j, f);
+        }
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            g[id++] = this->getF(i, j+1, f)-this->getF(i, j-1, f);
+        }
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            g[id++] = this->getF(i+1, j+1, f)-this->getF(i-1, j+1, f)-this->getF(i+1, j-1, f)+this->getF(i-1, j-1, f);
+        }
+    }
+}
+
+
+double BaseProcessor::bicubicInterpolation(double a, double b, double f[][4]) {
+    double alpha[4][4];
+    double g[16];
+    this->calculateFeatureVector(f, g);
+    for (int i = 0; i < 16; ++i) {
+        int i_x = i/4;
+        int i_y = i%4;
+        alpha[i_x][i_y] = 0.0;
+        for (int j = 0; j < 16; ++j) {
+            alpha[i_x][i_y] += BICUBIC_INTERPOLATION_INV_MATRIX[i][j] * g[j];
+        }
+    }
+    double result = 0;
+    double pa = 1.0;
+    for (int i = 0; i < 4; ++i) {
+        double pb = 1.0;
+        for (int j = 0; j < 4; ++j) {
+            result += pa * pb * alpha[i][j];
+            pb *= b;
+        }
+        pa *= a;
+    }
+    return result;
+}
+
+
 void BaseProcessor::pixelInterpolation(AffineTransform* r, Mat* result) {
     int W = this->image.cols;
     int H = this->image.rows;
-    if (r->getX() < W-1 && r->getX() > 0 && r->getY() < H-1 && r->getY() > 0) {
-        double x = r->getX();
-        double y = r->getY();
-        int l = trunc(x);
-        int k = trunc(y);
-        double a = x - l;
-        double b = y - k;
+    double x = r->getX();
+    double y = r->getY();
+    int l = trunc(x);
+    int k = trunc(y);
+    double a = x - l;
+    double b = y - k;
+    if (l <= W-2 && l >= 1 && k <= H-2 && k >= 1 && !isEqualDouble(l, x) && !isEqualDouble(k, y)) {
+        double f[4][4];
+        for (int c = 0; c < 3; ++c) {
+            for (int u = l-1; u <= l+2; ++u) {
+                for (int v = k-1; v <= k+2; ++v) {
+                    f[u-l+1][v-k+1] = this->image.at<Vec3b>(v, u)[c];
+                }
+            }
+            int option_1 = cvRound(this->bicubicInterpolation(a, b, f));
+            if (option_1 < 0) {
+                option_1 = 0;
+            }
+            else if (option_1 > 255) {
+                option_1 = 255;
+            }
+            result->at<Vec3b>(r->getIntOriginY(), r->getIntOriginX())[c] = option_1;
+        }
+    }
+    else if (l < W-1 && l >= 0 && k < H-1 && k >= 0) {
         for (int c = 0; c < 3; ++c) {
             result->at<Vec3b>(r->getIntOriginY(), r->getIntOriginX())[c]
                 = cvRound(
--- src/BaseProcessor.h
+++ src/BaseProcessor.h
@@ -9,6 +9,31 @@ private:
 protected:
     Mat image;
     virtual Mat processImage(argv_t kwargs);
+    double getF(int x, int y, double f[][4]);
+    void calculateFeatureVector(double f[][4], double g[16]);          // Here we use f as f(x, y), x is on horizontal axis while y is on vertical axis
+                                                                    // f shoule be able to index as follows:
+                                                                    //     f | -1 | 0 | 1 | 2  |
+                                                                    //    ---------------------|
+                                                                    //    -1 |    |   |   |    |
+                                                                    //    ---------------------|
+                                                                    //     0 |    |   |   |    |
+                                                                    //    ---------------------|
+                                                                    //     1 |    |   |   |    |
+                                                                    //    ---------------------|
+                                                                    //     2 |    |   |   |    |
+                                                                    //    ---------------------|
+    double bicubicInterpolation(double a, double b, double f[][4]);    // Here we use f as f(x, y), x is on horizontal axis while y is on vertical axis
+                                                                    // f shoule be able to index as follows:
+                                                                    //     f | -1 | 0 | 1 | 2  |
+                                                                    //    ---------------------|
+                                                                    //    -1 |    |   |   |    |
+                                                                    //    ---------------------|
+                                                                    //     0 |    |   |   |    |
+                                                                    //    ---------------------|
+                                                                    //     1 |    |   |   |    |
+                                                                    //    ---------------------|
+                                                                    //     2 |    |   |   |    |
+                                                                    //    ---------------------|
     void pixelInterpolation(AffineTransform* r, Mat* result);
 public:
     BaseProcessor(string pathToImage);
